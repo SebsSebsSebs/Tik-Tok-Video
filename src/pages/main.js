65a3789a9ebd86e30f7e491443766cd305cab22a
SebsSebsSebs <seberasmus@gmail.com>
2021-04-26 14:36:17 +0200

    add use effect

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Video from "../component/Video";
 import "../App.css";
 
-function main() {
+function Main() {
+  const [videos, setVideos] = useState([]);
+  console.log(videos);
+
+  useEffect(() => {
+    async function fetch() {
+      const res = await axios.get("http://localhost:5000/video");
+      setVideos(res.data);
+      return res;
+    }
+    fetch();
+  }, []);
+
   return (
     <div className="main">
       <div className="app_videos">
-        <Video />
-        
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
 }
 
-export default main;
+export default Main;
